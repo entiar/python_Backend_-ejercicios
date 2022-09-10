3f7b58bf0eda41d53310788c896dc36f1d29fcf7
entiar <87098963+entiar@users.noreply.github.com>
2022-09-09 23:45:11 -0400

    Add files via upload

---
+++ fichero.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def main():
+    pass
+def suma(a,b):
+    return a + b
+def resta(a,b):
+    return a - b
+def multiplicar(a,b):
+    return a*b
+def dividir(a,b):
+    return a/b
---
+++ mdulo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 21:05:39 2022
+
+@author: orion
+"""
+import fichero
+
+fichero.suma(3, 3)
+fichero.resta(4, 22)
+fichero.multiplicar(44, 3)
+fichero.dividir(22, 2)
